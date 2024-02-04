edb55c491d1e8ac38c99b50d9e332b1f061d7b15
Alex Estrada <estrada.alex20@gmail.com>
2024-02-04 12:58:44 -0800

    Adding animations to player
    Improving the scene and adding run animation to player. Followed this guide for animation. https://www.youtube.com/watch?v=GChUpPnOSkg&t=749s

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+
     [SerializeField] public float speed = 7f;
     [SerializeField] public float jumpHeight = 5f;
+
+    private float dirX = 0f;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
@@ -25,5 +33,27 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
         }
 
+        updateAnimationUpdate();
+        
+    }
+
+    private void updateAnimationUpdate()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
     }
 }
+
+
